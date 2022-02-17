a27ba3f63c4c56a9e5a42983f0efe0eb3fe8f648
kdbswo <75184363+kdbswo@users.noreply.github.com>
2022-02-18 07:52:47 +0900

    #7.1 To Do list
    map()

--- src/App.js
+++ src/App.js
@@ -4,20 +4,37 @@ import { useState } from 'react';
 
 function App() {
   const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
   const onChange = (event) => setToDo(event.target.value);
-  console.log(toDo);
-  return <div>
-    <form>
-      <input 
-        onChange={onChange} 
-        value={toDo} 
-        type="text" 
-        placeholder='write your to do...'
-      />
-      <button>Add To Do</button>
-    </form>
-  </div>
-   
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
+  console.log(toDos);
+  return (
+    <div>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input 
+          onChange={onChange} 
+          value={toDo} 
+          type="text" 
+          placeholder='write your to do...'
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item,index) => (
+          <li key={index}>{}{item}</li>
+        ))}
+      </ul>
+    </div>
+  )
 }
 
 export default App;
