9ee8f6d25d0ff39a99b87d995b68fcdad120d97d
kdbswo <75184363+kdbswo@users.noreply.github.com>
2022-03-01 18:51:20 +0900

    movie.1.0

--- src/App.js
+++ src/App.js
@@ -4,30 +4,14 @@ import { useEffect, useState } from 'react';
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-    .then((response) => response.json())
-    .then((json) => {
-      setCoins(json);
-      setLoading(false);
-    });
+    fetch(
+      'https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year'
+    ).then((response) => response.json()).then(json => console.log());
   }, []);
-  return (
-    <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`} </h1>
-      {loading ? (
-      <strong>Loading...</strong> 
-      ) : (
-        <select>
-          {coins.map((coin) => (
-            <option>{coin.name} ({coin.symbol}):${coin.quotes.USD.price} USD
-          </option>
-          ))}
-        </select>
-      )}
-    </div>
-  );
+  return <div>
+    {loading ? <h1>Loading...</h1> : null}
+  </div>;
 }
 
 export default App;
