8706f06946765781025870993d13c00ea6abf6a4
kdbswo <75184363+kdbswo@users.noreply.github.com>
2022-04-01 20:54:58 +0900

    movie1.6
    router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { 
+  BrowserRouter as Router, 
+  Switch, 
+  Route 
+} from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from './routes/Detail';
 
 function App() {
-  return null;
+  return (
+    <Router>
+      <Switch>
+        <Route path={"/movie"}>
+          <Detail />
+        </Route>
+        <Route path={"/"}>
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,6 +1,6 @@
 function Detail(){
     
-    return <h1>Detail</h1>
+    return <h1>Detail</h1>;
 }
 
 export default Detail;
