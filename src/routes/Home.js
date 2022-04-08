e19f3229218f13f1bb85eb06d749a11ca230e9fa
kdbswo <75184363+kdbswo@users.noreply.github.com>
2022-04-09 04:31:16 +0900

    movie2.0
    css

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import Movie from '../components/Movie';
+import styles from "./Home.module.css";
 
 function Home(){
     const [loading, setLoading] = useState(true);
@@ -17,15 +18,17 @@ function Home(){
     getMovies();
   }, []);
   console.log(movies);
-  return <div>
-    {loading ?
+  return (
+  <div className={styles.container}>
+    {loading ? (
      <h1>Loading...</h1>
-      : 
-      <div>
+    ) : ( 
+      <div className={styles.movies}>
         {movies.map((movie) => (
           <Movie 
             key={movie.id}
-            id={movie.id} 
+            id={movie.id}
+            year={movie.year} 
             coverImg={movie.medium_cover_image}
             title={movie.title}
             summary={movie.summary}
@@ -33,8 +36,9 @@ function Home(){
           />
         ))}
       </div>
-    }
-  </div>;
+    )}
+  </div>
+  );
 }
 
 export default Home;
